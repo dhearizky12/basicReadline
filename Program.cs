9e0d49c2f933693d8db91ddf87e57d3b5a3c1986
dhearizky12 <dherizdhea@gmail.com>
2023-04-11 23:50:44 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿class Program {
+   static void Main () {
+
+       Console.Write("input your name = ");
+       string? userInputName = Console.ReadLine();
+       Console.Write("input your age = ");
+       string? userInputAge = Console.ReadLine();
+
+
+    Console.WriteLine("Hello, "+ userInputName + " ! " + "You are " + userInputAge + " year");
+   //  Console.WriteLine ( $"Hello, {userInputName} {userInputAge}");
+    }
+}
